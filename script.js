cf90dad09834e39f59cc98550b069ce5b2cde8aa
Prince Bhatt <prince25io044@satiengg.in>
2024-02-05 15:23:51 +0530

    Testing Page3

--- script.js
+++ script.js
@@ -101,3 +101,24 @@ tl.from('.sidebarlogoss img', {
   stagger: 0.5,
   opacity: 0,
 });
+
+const myNum = document.querySelectorAll('.count');
+let speed = 500;
+
+myNum.forEach(myCount => {
+  let target_count = myCount.dataset.count;
+  let init_count = +myCount.innerText;
+  let new_increment_num = Math.floor(target_count / speed);
+
+  const updateNumber = () => {
+    init_count += new_increment_num;
+    myCount.innerText = init_count;
+
+    if (init_count < target_count) {
+      setTimeout(() => {
+        updateNumber();
+      }, 5);
+    }
+  };
+  updateNumber();
+});
